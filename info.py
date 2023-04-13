5cc4bc58122583249b759f0fb322124f59665e00
Kk2020k <108136211+Kk2020k@users.noreply.github.com>
2023-04-13 19:38:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -82,7 +82,7 @@ if SELF_DELETE == "True":
 
     # Download Tutorial Button #
 DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
-DOWNLOAD_TEXT_URL = "https://t.me/MainChannel2k/6"
+DOWNLOAD_TEXT_URL = "https://t.me/MainChannal2k/3"
 
    # Custom Caption Under Button #
 CAPTION_BUTTON = "Updates"
